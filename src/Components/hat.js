2afa092a092b3168c06d41b1f9e5bddb524197c8
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-05-09 17:44:53 -0400

    hatEditForm created and coded

---
+++ src/Components/hat.js
@@ -0,0 +1,25 @@
+ // import { useState } from 'react';
+
+
+// function Hat ({ hat}) {
+// return(
+
+//     <tr>
+//         <td>{hat.style}</td>
+//         <td>{hat.color}</td> 
+//         <td>{hat.size}</td>
+        // <td>{hat.is_available}</td>
+//         <td>{hat.material}</td>  
+//         <td>{hat.price}</td> 
+
+//         <td>     
+//        <button onClick={() => editHat(hat.id)}>Edit</button>
+//         </td>
+//         <td>
+/* <button onClick={() => deleteHat(hat.id)}>Delete</button */
+//         </td>
+//     </tr>
+// )
+// }
+// export default Hat;
+
