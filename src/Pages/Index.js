1fd1e932c6bb54e6a3048b3c258ca205aabf1e0b
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-09 17:50:39 -0400

    index

---
+++ src/Pages/Index.js
@@ -0,0 +1,10 @@
+import Hats from "../Components/Hats";
+
+export default function Index() {
+    return (
+        <div className="Index">
+            <h2>Index</h2>
+            <Hats />
+        </div>
+    );
+};
