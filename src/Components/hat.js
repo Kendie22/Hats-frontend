a66c4b8f83ced6b7b9a5c06f1ea52aa384a79ea7
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-10 17:57:31 -0400

    fixing components

--- src/Components/hat.js
+++ src/Components/hat.js
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
 
 
-function Hat ({ hat }) {
-return(
+function Hat({ hat }) {
+    return (
 
-    <tr>
-        <td>{hat.style}</td>
-        <td>{hat.color}</td> 
-        <td>{hat.size}</td>
-        <td>{hat.is_available}</td>
-        <td>{hat.material}</td>  
-        <td>{hat.price}</td> 
+        <tr>
+            <td>{hat.style}</td>
+            <td>{hat.color}</td>
+            <td>{hat.size}</td>
+            <td>{hat.is_available}</td>
+            <td>{hat.material}</td>
+            <td>{hat.price}</td>
 
-        <td>     
-     <Link to={`/hats/${hat.id}`}>✏️</Link>
-        </td>
-       
-    </tr>
-)
+            <td>
+                <Link to={`/hats/${hat.id}`}>✏️</Link>
+            </td>
+
+        </tr>
+    )
 }
 export default Hat;
 
