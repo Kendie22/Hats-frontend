c60fb451be65f843ca52d2a5eaf4af05d04e6273
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-05-10 18:01:42 -0400

    Edit page

---
+++ src/Pages/Edit.js
@@ -0,0 +1,12 @@
+import HatEditForm from "./Components/HatEditForm";
+
+function Edit() {
+  return (
+    <div className="New Edit">
+      <h2>Edit</h2>
+      <HatEditForm />
+    </div>
+  );
+}
+
+export default Edit;
