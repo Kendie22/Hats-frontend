a31b69abb1a2ed33ee8a0ba7c675898b9b54d6c6
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-10 17:45:09 -0400

    renamed components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // PAGES
-import Edit from "./Pages/Edit";
-import FourOFour from "./Pages/FourOFour";
+// import Edit from "./Pages/Edit";
+// import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
 import Index from "./Pages/Index";
-import New from "./Pages/New";
+// import New from "./Pages/New";
 import Show from "./Pages/Show";
 
 // COMPONENTS
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/Nav.Bar";
 
 function App() {
     return (
@@ -23,10 +23,10 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/hats" element={<Index />} />
-                        <Route path="/hats/new" element={<New />} />
+                        {/* <Route path="/hats/new" element={<New />} /> */}
                         <Route exact path="/hats/:id" element={<Show />} />
-                        <Route path="/hats/:id/edit" element={<Edit />} />
-                        <Route path="*" element={<FourOFour />} />
+                        {/* <Route path="/hats/:id/edit" element={<Edit />} /> */}
+                        {/* <Route path="*" element={<FourOFour />} /> */}
                     </Routes>
                 </main>
             </Router>
--- src/Components/Hats.js
+++ src/Components/Hats.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Hat from "./Hat";
+import Hat from "./hat";
 
 const API = process.env.REACT_APP_API_URL;
 
