f3df5cb22f1285e642cdba08e6e77b2a364c416c
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-10 10:33:21 -0400

    uncommented

--- src/Components/Nav.Bar.js
+++ src/Components/Nav.Bar.js
@@ -1,14 +1,14 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-// export default function NavBar() {
-//   return (
-//     <nav>
-//       <h1>
-//         <Link to="/hats">Hats</Link>
-//       </h1>
-//       <button>
-//         <Link to="/hats/new">New Hats</Link>
-//       </button>
-//     </nav>
-//   );
-// }
+export default function NavBar() {
+    return (
+        <nav>
+            <h1>
+                <Link to="/hats">Hats</Link>
+            </h1>
+            <button>
+                <Link to="/hats/new">New Hats</Link>
+            </button>
+        </nav>
+    );
+}
