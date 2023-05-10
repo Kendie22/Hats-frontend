0e36ae8a9fefc08181d40fc13ceee0ad06b05a73
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-10 10:48:22 -0400

    uncommented

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // PAGES
-// import Edit from "./Pages/Edit";
-// import FourOFour from "./Pages/FourOFour";
-// import Home from "./Pages/Home";
-// import Index from "./Pages/Index";
-// import New from "./Pages/New";
-// import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
 
 // COMPONENTS
-// import NavBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Router>
-//         <NavBar />
-//         <main>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/hats" element={<Index />} />
-//             <Route path="/hats/new" element={<New />} />
-//             <Route exact path="/hats/:id" element={<Show />} />
-//             <Route path="/hats/:id/edit" element={<Edit />} />
-//             <Route path="*" element={<FourOFour />} />
-//           </Routes>
-//         </main>
-//       </Router>
-//     </div>
-//   );
-// }
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <NavBar />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/hats" element={<Index />} />
+                        <Route path="/hats/new" element={<New />} />
+                        <Route exact path="/hats/:id" element={<Show />} />
+                        <Route path="/hats/:id/edit" element={<Edit />} />
+                        <Route path="*" element={<FourOFour />} />
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    );
+}
 
-// export default App;
+export default App;
