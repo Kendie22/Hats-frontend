1ba88b112d80556fc667d5da83f65e084f434ab0
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-12 11:42:25 -0400

    added hover and gradient

--- src/Components/hat.js
+++ src/Components/hat.js
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 
 function Hat({ hat }) {
+    console.log(hat.image)
     return (
         <tr>
             <td>
                 <Link to={`/hats/${hat.id}`}>
                     <article className="hat-image">
-                        <img src={hat.image} alt="hat pic" />
+                        <img src={hat.image} alt="hat-pic" />
                         <h3>{hat.style}</h3>
                     </article>
                 </Link>
